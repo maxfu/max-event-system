7828a0310b450096fc0df85ca415ed754d0cbb62
Xiaoye Fu <xiaoye.fu@outlook.com.au>
2019-03-29 02:44:08 +0000

    Removed all php for debug

--- templates/chamber-event.php
+++ templates/chamber-event.php
@@ -1,6 +1,3 @@
-<?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; ?>
-<?php $custom_loop = new WP_Query(array( 'post_type' => 'event', 'posts_per_page' => 20, 'category_name' => 'chamber', 'meta_key' => 'event_begin', 'orderby' => 'meta_value_num', 'order' => 'DESC' )); ?>
-<?php while ( $custom_loop->have_posts() ) : $custom_loop->the_post(); ?>
 <div class="event_item media-container-row pt-5 pb-3">
     <div class="mbr-figure" style="width: 40%;">
         <a href=""><img src=""></a>
@@ -13,6 +10,3 @@
         </div>
     </div>
 </div>
-<?php endwhile; ?>
-// <?php if (function_exists("ccca_pagination")) { ccca_pagination($custom_loop->max_num_pages); } ?>
-<?php wp_reset_postdata(); ?>
