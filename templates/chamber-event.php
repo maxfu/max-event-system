d6783df68b74872ef018d4b1d742bd5cea16dc82
Xiaoye Fu <xiaoye.fu@outlook.com.au>
2019-03-29 02:44:40 +0000

    More php removed

--- templates/chamber-event.php
+++ templates/chamber-event.php
@@ -3,7 +3,7 @@
         <a href=""><img src=""></a>
     </div>
     <div class="align-left aside-content">
-        <h2 class="mbr-title mbr-fonts-style display-3"><a href=""><?php the_title(); ?></a></h2>
+        <h2 class="mbr-title mbr-fonts-style display-3"><a href="">Title</a></h2>
         <div class="mbr-section-text">
             <p class="mbr-text mt-3 mbr-light mbr-fonts-style display-5 fixed-height"></p>
             <p class="mbr-text mbr-light mbr-fonts-style display-5 view-more">...<a href="">了解更多</a></p>
