bf4d45d69a39b4f56be117c2d278615e8177f805
Xiaoye Fu <xiaoye.fu@outlook.com.au>
2019-03-29 02:35:51 +0000

    Debuging

--- templates/chamber-event.php
+++ templates/chamber-event.php
@@ -3,7 +3,7 @@
 <?php while ( $custom_loop->have_posts() ) : $custom_loop->the_post(); ?>
 <div class="event_item media-container-row pt-5 pb-3">
     <div class="mbr-figure" style="width: 40%;">
-        <a href="<?php echo esc_url(get_permalink()); ?>"><img src="<?php if ( has_post_thumbnail() ) { the_post_thumbnail_url('full'); } else { echo get_template_directory_uri() . '/assets/images/1.jpg'; } ?>"></a>
+//        <a href="<?php echo esc_url(get_permalink()); ?>"><img src="<?php if ( has_post_thumbnail() ) { the_post_thumbnail_url('full'); } else { echo get_template_directory_uri() . '/assets/images/1.jpg'; } ?>"></a>
     </div>
     <div class="align-left aside-content">
         <h2 class="mbr-title mbr-fonts-style display-3"><a href="<?php echo esc_url(get_permalink()); ?>"><?php the_title(); ?></a></h2>
