0ac8a1beae8e150aa6bfed6df39778ec1cec7893
Xiaoye Fu <xiaoye.fu@outlook.com.au>
2019-03-30 12:31:35 +0000

    Add tax_query to member-events

--- templates/member-events.php
+++ templates/member-events.php
@@ -1,6 +1,25 @@
-<?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; ?>
-<?php $custom_loop = new WP_Query(array( 'post_type' => 'event', 'posts_per_page' => 20, 'category_name' => 'member', 'meta_key' => 'event_begin', 'orderby' => 'meta_value_num', 'order' => 'DESC' )); ?>
-<?php while ( $custom_loop->have_posts() ) : $custom_loop->the_post(); ?>
+<?php
+$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+$args = array(
+    'post_type'         => array( 'event' ),
+    'nopaging'          => false,
+    'posts_per_page'    => '20',
+    'tax_query'         => array(
+        array(
+            'taxonomy'  => 'branch',
+            'include_children' => true,
+            'field' => 'slug',
+            'terms' => 'member',
+        ),
+    ),
+    'meta_key' => 'event_begin',
+    'orderby' => 'meta_value_num',
+    'order' => 'DESC',
+);
+
+$custom_loop = new WP_Query( $args);
+while ( $custom_loop->have_posts() ) : $custom_loop->the_post();
+?>
 <div class="event_item media-container-row pt-5 pb-3">
     <div class="mbr-figure" style="width: 40%;">
         <a href="<?php echo esc_url(get_permalink()); ?>"><img src="<?php if ( has_post_thumbnail() ) { the_post_thumbnail_url('full'); } else { echo get_template_directory_uri() . '/assets/images/1.jpg'; } ?>"></a>
@@ -13,6 +32,8 @@
         </div>
     </div>
 </div>
-<?php endwhile; ?>
-<?php if (function_exists("ccca_pagination")) { ccca_pagination($custom_loop->max_num_pages); } ?>
-<?php wp_reset_postdata(); ?>
+<?php
+endwhile;
+if (function_exists("ccca_pagination")) { ccca_pagination($custom_loop->max_num_pages); }
+wp_reset_postdata();
+?>
