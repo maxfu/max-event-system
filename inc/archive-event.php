53372e7cfa032afdb7f0b26d40833c70487bda63
Xiaoye Fu <xiaoye.fu@outlook.com.au>
2019-03-31 11:35:21 +0000

    Modify Archive Page

--- inc/archive-event.php
+++ inc/archive-event.php
@@ -20,10 +20,10 @@
 	</section>
 
 	<!-- section -->
-		<section class="features11 cid-qSShVnZyJK" id="content4-p">
-      <div class="container">
-        <div class="media-container-row">
-          <div class="mbr-text col-12 mbr-fonts-style display-7">
+	<section class="features11 cid-qSShVnZyJK" id="content4-p">
+        <div class="container">
+            <div class="media-container-row">
+                <div class="mbr-text col-12 mbr-fonts-style display-7">
 
 						<?php $args = array(
 							'post_status' => 'publish',
@@ -39,30 +39,24 @@
 							<?php $event_end_date = get_post_meta( get_the_ID(), 'event_end', true ); ?>
 							<?php $event_venue = get_post_meta( get_the_ID(), 'event-venue', true ); ?>
 							<?php $event_rsvp = get_post_meta( get_the_ID(), 'event-rsvp', true ); ?>
-							<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-								<div class="media-container-row pt-5 pb-3 mt-3 mb-3">
+								<div class="media-container-row pt-5 pb-3 mt-3 mb-3" id="post-<?php the_ID(); ?>">
 							      <div class="mbr-figure" style="width: 40%;">
-							        <?php if ( has_post_thumbnail() ) {
-							        the_post_thumbnail();
-							        } else { ?>
+							        <?php if ( has_post_thumbnail() ) { ?>
+							        <?php the_post_thumbnail(); ?>
+							        <?php } else { ?>
 							        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/mbr-1-1200x800.jpg" alt="<?php the_title(); ?>" title="">
 							        <?php } ?>
 							      </div>
 							      <div class="align-left aside-content">
 							          <h2 class="mbr-title pt-2 mbr-fonts-style display-3"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
 							          <div class="mbr-section-text">
-							              <p class="mbr-text mb-5 pt-3 mbr-light mbr-fonts-style display-5">
-															<span class="date"><?php echo 'Start Date: ' . date( 'F d, Y', $event_start_date ); ?></span><br>
-															<span class="date"><?php echo 'End Date: ' . date( 'F d, Y', $event_end_date ); ?></span><br>
-															<span class="date"><?php echo 'Venue: ' . $event_venue; ?></span><br>
-															<span class="date"><?php echo 'RSVP: ' . $event_rsvp; ?></span>
-															<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
-														</p>
+                                        <p class="mbr-text mb-5 pt-3 mbr-light mbr-fonts-style display-5">
+                                            <span class="date"><?php echo __('Start Date: ', 'max-event' ) . date( 'F d, Y', $event_start_date ); ?></span><br>
+											<span class="date"><?php echo __('End Date: ', 'max-event' ) . date( 'F d, Y', $event_end_date ); ?></span><br>
+										</p>
 							          </div>
 							      </div>
 							    </div>
-								</article>
-								<!-- /article -->
 							<?php endwhile; wp_reset_postdata(); ?>
 						<?php else: ?>
 							<!-- article -->
@@ -71,10 +65,10 @@
 							</article>
 							<!-- /article -->
 						<?php endif; ?>
+			            <?php get_template_part('pagination'); ?>
 					</div>
 				</div>
 			</div>
-			<?php get_template_part('pagination'); ?>
 		</section>
 		<!-- /section -->
 	</main>
