7e3b12976f81292bc015049155f0513262aece8b
Xiaoye Fu <xiaoye.fu@outlook.com.au>
2019-04-03 12:28:23 +0000

    Event Post

--- inc/single-event.php
+++ inc/single-event.php
@@ -1,67 +1,3 @@
-<?php
-
-  //response generation function
-
-  $response = "";
-
-  //function to generate response
-  function my_contact_form_generate_response($type, $message){
-
-    global $response;
-
-    if($type == "success") $response = "<div class='success'>{$message}</div>";
-    else $response = "<div class='error'>{$message}</div>";
-
-  }
-
-  //response messages
-  $not_human       = "Human verification incorrect.";
-  $missing_content = "Please supply all information.";
-  $email_invalid   = "Email Address Invalid.";
-  $message_unsent  = "Message was not sent. Try Again.";
-  $message_sent    = "Thanks! Your message has been sent.";
-
-  //user posted variables
-  $name = $_POST['message_name'];
-  $email = $_POST['message_email'];
-  $message = $_POST['message_text'];
-  $human = $_POST['message_human'];
-
-  //php mailer variables
-  // $to = get_option('admin_email');
-	$to = $_POST['message_email'];
-  $subject = "Someone sent a message from ".get_bloginfo('name');
-  $headers = 'From: '. $email . "\r\n" .
-    'Reply-To: ' . $email . "\r\n";
-
-  if(!$human == 0){
-    if($human != 2) my_contact_form_generate_response("error", $not_human); //not human!
-    else {
-
-      //validate email
-      if(!filter_var($email, FILTER_VALIDATE_EMAIL))
-        my_contact_form_generate_response("error", $email_invalid);
-      else //email is valid
-      {
-        //validate presence of name and message
-        if(empty($name) || empty($message)){
-          my_contact_form_generate_response("error", $missing_content);
-        }
-        else //ready to go!
-        {
-          $sent = wp_mail($to, $subject, strip_tags($message), $headers);
-          if($sent) {
-						my_contact_form_generate_response("success", $message_sent); //message sent!
-						my_contact_form_generate_response("success", $_POST['message_email']); //message sent!
-					}
-          else my_contact_form_generate_response("error", $message_unsent); //message wasn't sent
-        }
-      }
-    }
-  }
-  else if ($_POST['submitted']) my_contact_form_generate_response("error", $missing_content);
-?>
-
 <?php get_header(); ?>
 
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
@@ -101,10 +37,8 @@
           </div>
         <?php } else { ?>
           <div class="media-container-row">
-  			<div class="mbr-text col-8 col-md-8 mbr-fonts-style display-7">
-              <p>
-                <?php _e($event_details); ?>
-              </p>
+  			<div class="mbr-text col-12 mbr-fonts-style display-7">
+              <?php the_content(); ?>
   			  <p>
                 <?php echo 'Event Start Date: ' . date( 'd/m/Y g:i A', $event_start_date ); ?><br>
                 <?php echo 'Event End Date: ' . date( 'd/m/Y g:i A', $event_end_date ); ?><br>
@@ -112,12 +46,10 @@
               </p>
   			</div>
   		  </div>
-  				<?php if ( $event_rsvp == 'All' ) { ?>
+  				<?php if ( $event_rsvp == 'Yes' ) { ?>
   					<div class="media-container-row">
   						<div class="mbr-text col-4 col-md-4 mbr-fonts-style display-7">
-                <?php echo $event_rsvp_link; ?><br>
-                <?php $homepage = file_get_contents( $event_rsvp_link );
-                echo $homepage; ?>
+                          <?php echo $event_rsvp_link; ?><br>
   						</div>
   					</div>
   				<?php } ?>
