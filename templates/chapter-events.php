3a4efd6ba66c8e5ff327ed76c4b52cf28480f339
Xiaoye Fu <xiaoye.fu@outlook.com.au>
2019-04-03 11:07:35 +0000

    Upcoming events to shortcake

--- templates/chapter-events.php
+++ templates/chapter-events.php
@@ -19,6 +19,61 @@ $args = array(
             'include_children' => true,
         ),
     ),
+    'meta_query' => array(
+        array(
+            'key'     => 'event_begin',
+            'value'   => (int)strtotime("now"),
+            'compare' => '>',
+            'type'    => 'NUMERIC',
+        ),
+    ),
+    'meta_key' => 'event_begin',
+    'orderby' => 'meta_value_num',
+    'order' => 'DESC',
+);
+
+$custom_loop = new WP_Query( $args);
+while ( $custom_loop->have_posts() ) : $custom_loop->the_post();
+?>
+<div class="event_item media-container-row pt-5 pb-3">
+    <div class="mbr-figure" style="width: 40%;">
+        <a href="<?php echo esc_url(get_permalink()); ?>"><img src="<?php if ( has_post_thumbnail() ) { the_post_thumbnail_url('full'); } else { echo get_template_directory_uri() . '/assets/images/1.jpg'; } ?>"></a>
+    </div>
+    <div class="align-left aside-content">
+        <h2 class="mbr-title mbr-fonts-style display-3"><a href="<?php echo esc_url(get_permalink()); ?>"><?php the_title(); ?></a></h2>
+        <div class="mbr-section-text">
+            <p class="mbr-text mt-3 mbr-light mbr-fonts-style display-5 fixed-height"><?php ccca_the_excerpt('ccca_excerpt'); ?> </p>
+            <p class="mbr-text mbr-light mbr-fonts-style display-5 view-more">...<a href="<?php echo esc_url(get_permalink()); ?>">了解更多</a></p>
+        </div>
+    </div>
+</div>
+<?php
+endwhile;
+if (function_exists("ccca_pagination")) { ccca_pagination($custom_loop->max_num_pages); }
+wp_reset_postdata();
+?>
+
+<?php
+$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+$args = array(
+    'post_type'         => array( 'event_moment' ),
+    'nopaging'          => false,
+    'posts_per_page'    => '20',
+    'tax_query'         => array(
+        'relation' => 'OR',
+        array(
+            'taxonomy'         => 'branch',
+            'terms'            => 'branch',
+            'field'            => 'slug',
+            'include_children' => true,
+        ),
+        array(
+            'taxonomy'         => 'branch',
+            'terms'            => 'committee',
+            'field'            => 'slug',
+            'include_children' => true,
+        ),
+    ),
     'meta_key' => 'event_begin',
     'orderby' => 'meta_value_num',
     'order' => 'DESC',
